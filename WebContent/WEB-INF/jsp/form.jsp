32e25a34ab69603feceef978139032377ad866e8
Vince <vincent.tournier@etu.hesge.ch>
2016-11-19 16:17:37 +0100

    JSP are ready to run, only need loops

--- WebContent/WEB-INF/jsp/form.jsp
+++ WebContent/WEB-INF/jsp/form.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -15,6 +16,7 @@
 	       		<input type="text" name="start" id="start" />
 	       		<br />
 	       		<input type="submit" value="Cul !" />
+	       		<!-- ID OF FIELDS TO ADD TO THE FORM : -->
 			 </p>
 		</form>
 	</body>
--- src/ch/lauzhack/triphub/servlet/RequestReceiver.java
+++ src/ch/lauzhack/triphub/servlet/RequestReceiver.java
@@ -7,12 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(description = "Get request and launches algorithm", urlPatterns = { "/request" })
+//@WebServlet(description = "Get request and launches algorithm", urlPatterns = { "/request" })
 public class RequestReceiver extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	
-    public RequestReceiver() {
+    public RequestReceiver() 
+    {
         super();
     }
 
